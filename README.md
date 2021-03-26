d643a9d662b51d7059a7fa8d949a37fdbb79bbde
isoletslicer <ardogible@gmail.com>
2021-03-27 06:11:38 +0700

    layout tweaks  & dark mode implementations

---
+++ README.md
@@ -0,0 +1,7 @@
+A Simple Restaurant Rater App.
+
+This repo is based on [Sanjeev Thiyagarajan's PERN Stack CRUD Yelp Clone Tutorial](https://www.youtube.com/watch?v=J01rYl9T3BU&t=1865s). Thank you for making this awesome PERN-Stack Tutorial !
+
+Check out this [Dark Mode Toggle Tutorial](https://www.smashingmagazine.com/2020/04/dark-mode-react-apps-styled-components/).
+
+This version contains about setting up for deployment in Heroku, a bit tweaks, and dark-mode implementation using styled-components.
---
+++ client/src/App.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/GlobalStyles";
+import { lightTheme, darkTheme } from "./components/Themes"
+import  {useDarkMode} from "./components/useDarkMode"
+import Toggle from "./components/Toggler"
+
+
+const App = () => {
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  if(!mountedComponent) return <div/>
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <>
+      <GlobalStyles/>
+    <RestaurantsContextProvider>
+      <div className="container">
+      <Toggle theme={theme} toggleTheme={themeToggler} />
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              component={UpdatePage}
+            />
+            <Route
+              exact
+              path="/restaurants/:id"
+              component={RestaurantDetailPage}
+            />
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+    </>
+    </ThemeProvider>
+
+  );
+};
+
+export default App;
