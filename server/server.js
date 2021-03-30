d1efc290ea9addda4d0f3e78b42e6a40b7feba00
isoletslicer <ardogible@gmail.com>
2021-03-31 05:49:56 +0700

    coba lagi 3

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
+const port = process.env.PORT || 3001;
 
 const morgan = require("morgan");
 
@@ -9,6 +10,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+if (process.env.NODE_ENV === "production") {
+  //server static content
+  //npm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+
+
 
 // Get all Restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
@@ -136,7 +147,13 @@ app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3001;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+
+
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
 });
